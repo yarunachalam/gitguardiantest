8b6d56f5ef104e88024bdfcfae2c1ab35ef0273b
yarunachalam <yarunachalam@suse.com>
2023-08-09 14:47:01 -0500

    insert config hook

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 [default]
-aws_access_key_id = AKIAIOSFODNN7EXAMPLE
+aws_access_key_id = AKIAIOSFODNN7EXAMPLTEE
 aws_secret_access_key = wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY 
 region = us-east-2
